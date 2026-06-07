e42429188c60fcb14a72bd38866aa6bfe927a12e
Nexa Connect <karlprime76@gmail.com>
2026-06-07 02:40:59 +0100

    Make migration idempotent with IF NOT EXISTS

--- prisma/migrations/20240607_add_referral_code/migration.sql
+++ prisma/migrations/20240607_add_referral_code/migration.sql
@@ -1,5 +1,7 @@
 -- Add referralCode column to User table
-ALTER TABLE "User" ADD COLUMN "referralCode" TEXT;
+ALTER TABLE "User"
+ADD COLUMN IF NOT EXISTS "referralCode" TEXT;
 
 -- Add unique constraint on referralCode
-CREATE UNIQUE INDEX "User_referralCode_key" ON "User"("referralCode");
+CREATE UNIQUE INDEX IF NOT EXISTS "User_referralCode_key"
+ON "User"("referralCode");
